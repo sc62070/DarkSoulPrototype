a6d826f37477a0b470cd3b0a27cdfdb571528f3e
David Rochin <jdrc8@hotmail.com>
2019-05-02 20:23:42 -0600

    Made CharacterAI much more intelligent. Now it is state-based.

--- Assets/Scripts/CharacterAI.cs
+++ Assets/Scripts/CharacterAI.cs
@@ -16,6 +16,10 @@ public class CharacterAI : MonoBehaviour {
     [Range(0f, 1f)]
     public float combatAggressiveness = 1f;
 
+    private float roundDirectionChangeMultiplier = 1f;
+
+    private bool shouldBlock = false;
+
     void Awake() {
         character = GetComponent<Character>();
         motor = GetComponent<CharacterMotor>();
@@ -23,13 +27,15 @@ public class CharacterAI : MonoBehaviour {
 
     void Start() {
         StartCoroutine(AIThinkingRoutine());
+        StartCoroutine(AIDirectionChangeRoutine());
+        StartCoroutine(AIBlockRoutine());
+        StartCoroutine(AIAttackRoutine());
     }
 
     void Update() {
+        if (character.photonView.IsMine && character.health > 0f) {
 
-        if (character.health > 0f) {
-
-            // Check for enemies
+            // Check for targets
             if (character.target == null) {
                 Collider[] cols = Physics.OverlapSphere(transform.position, 10f, LayerMask.GetMask(new string[] { "Characters" }), QueryTriggerInteraction.Ignore);
 
@@ -41,20 +47,51 @@ public class CharacterAI : MonoBehaviour {
                 }
             }
 
-            if (character.target != null) {
-                character.Block(true);
+            bool canReach = false;
+            NavMeshPath path = new NavMeshPath();
 
+            if (character.target != null) {
                 motor.TurnTowards((character.target.transform.position - transform.position));
 
-                if (Vector3.Distance(transform.position, character.target.transform.position) > 2f) {
-                    NavMeshPath path = new NavMeshPath();
-                    bool canReach = NavMesh.CalculatePath(transform.position, character.target.transform.position, NavMesh.AllAreas, path);
+                character.Block(shouldBlock);
+
+                // Calculate path to target
+                path = new NavMeshPath();
+                canReach = NavMesh.CalculatePath(transform.position, character.target.transform.position, NavMesh.AllAreas, path);
+            }
+
+            // Act according to state
+            switch (state) {
+                case AIState.Idle:
+                    break;
+                case AIState.ChasingTarget:
+                    if (character.target != null) {
+                        if (Vector3.Distance(transform.position, character.target.transform.position) > 2f) {
+
+                            /// Chase target
+                            if (canReach) {
+                                motor.Walk((path.corners[1] - transform.position).normalized);
+                            }
+                        } else {
+                            character.Attack(AttackType.Light);
+                        }
+                    }
+                    break;
+                case AIState.RoundingTarget:
+                    motor.Walk(transform.right * roundDirectionChangeMultiplier * combatSpeed);
+                    break;
+                case AIState.Attacking:
+
+                    /// Chase target
                     if (canReach) {
                         motor.Walk((path.corners[1] - transform.position).normalized);
                     }
-                } else {
-                    character.Attack(AttackType.Light);
-                }
+
+                    if (Vector3.Distance(transform.position, character.target.transform.position) < 2f) {
+                        character.Attack(AttackType.Light);
+                        state = AIState.RoundingTarget;
+                    }
+                    break;
 
             }
         }
@@ -63,7 +100,72 @@ public class CharacterAI : MonoBehaviour {
 
     private IEnumerator AIThinkingRoutine() {
         while (true) {
+            yield return new WaitForEndOfFrame();
 
+            // Logic to change states
+            switch (state) {
+                case AIState.Idle:
+                    if(character.target != null) {
+                        state = AIState.ChasingTarget;
+                    }
+                    break;
+                case AIState.ChasingTarget:
+
+                    // If close enough to target, start rounding him
+                    if(Vector3.Distance(transform.position, character.target.transform.position) <= 4f) {
+                        state = AIState.RoundingTarget;
+                    }
+                    break;
+                case AIState.RoundingTarget:
+                    /*yield return new WaitForSeconds(Random.Range(2f, 8f));
+
+                    if (character.target != null) {
+                        state = AIState.Attacking;
+                    } else {
+                        state = AIState.Idle;
+                    }*/
+                    break;
+            }
+        }
+    }
+
+    private IEnumerator AIDirectionChangeRoutine() {
+        while (true) {
+
+            yield return new WaitForSeconds(2f);
+            float[] values = new float[] { 1f, 0f, -1f};
+            roundDirectionChangeMultiplier = values[Random.Range(0, values.Length)];
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator AIAttackRoutine() {
+        while (true) {
+            if(state == AIState.RoundingTarget) {
+                yield return new WaitForSeconds(Random.Range(3f, 8f));
+
+                if (character.target != null) {
+                    state = AIState.Attacking;
+                } else {
+                    state = AIState.Idle;
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator AIBlockRoutine() {
+        while (true) {
+            yield return new WaitForSeconds(1f);
+
+            if (character.target != null && character.stamina / character.maxStamina > 0.8f){
+                shouldBlock = true;
+            } else {
+                shouldBlock = false;
+            }
+            
             yield return new WaitForEndOfFrame();
         }
     }
@@ -74,6 +176,7 @@ public enum AIState {
     Idle,
     Patroling,
     RunningAway,
+    ChasingTarget,
     RoundingTarget,
     AwaitingTargetMove,
     Attacking
